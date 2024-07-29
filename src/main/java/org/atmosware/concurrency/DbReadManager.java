adfb57eff704aca11949f86846842f29022241a5
yusakaynar <yusadev2024@gmail.com>
2024-07-29 13:43:58 +0300

    quick fix.

--- src/main/java/org/atmosware/concurrency/DbReadManager.java
+++ src/main/java/org/atmosware/concurrency/DbReadManager.java
@@ -1,44 +1,37 @@
 package org.atmosware.concurrency;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class DbReadManager {
 
     public static void startDbReadThreads() {
 
-        // Create and start threads for concurrent operations
-        Thread readUsersThread = new Thread(new DbReadTask("readUsers"));
-        Thread readAccountsThread = new Thread(new DbReadTask("readAccounts"));
-        Thread readAddressesThread = new Thread(new DbReadTask("readAddresses"));
+        try(ExecutorService executorService = Executors.newFixedThreadPool(3)) {
 
-        try {
-            readUsersThread.start();
-            readUsersThread.join();
-            readAccountsThread.start();
-            readAccountsThread.join();
-            readAddressesThread.start();
-            readAddressesThread.join();
-        } catch (InterruptedException e) {
+            executorService.execute(() -> new DbReadTask("readUsers"));
+            executorService.execute(() -> new DbReadTask("readAccounts"));
+            executorService.execute(() -> new DbReadTask("readAddresses"));
+
+        } catch(Exception e){
             e.printStackTrace();
         }
 
-        // synchronized ()
-        /*
+        //
 
-        Thread readUsersThread = new Thread(new DataReadTask("readUsers"));
-        Thread readAccountsThread = new Thread(new DataReadTask("readAccounts"));
-        Thread readAddressesThread = new Thread(new DataReadTask("readAddresses"));
-
-        try(ExecutorService executorService = Executors.newFixedThreadPool(3)) {
-
-            executorService.execute(() -> new DataReadTask("readUsers"));
-            executorService.execute(() -> new DataReadTask("readAccounts"));
-            executorService.execute(() -> new DataReadTask("readAddresses"));
-            // readUsersThread.start();
-            // readUsersThread.join();
-            // readAccountsThread.start();
-            // readAccountsThread.join();
-            // readAddressesThread.start();
-            // readAddressesThread.join();
-        }
-         */
+//        Thread readUsersThread = new Thread(new DbReadTask("readUsers"));
+//        Thread readAccountsThread = new Thread(new DbReadTask("readAccounts"));
+//        Thread readAddressesThread = new Thread(new DbReadTask("readAddresses"));
+//
+//        try {
+//            readUsersThread.start();
+//            readUsersThread.join();
+//            readAccountsThread.start();
+//            readAccountsThread.join();
+//            readAddressesThread.start();
+//            readAddressesThread.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 }
